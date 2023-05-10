2df7918c10c52a6d579fa36b2d373d78231da27f
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-05-10 17:54:10 -0300

    Finalizado a logica de JS e estilização

--- funcoes.js
+++ funcoes.js
@@ -1,45 +1,53 @@
 var areaTextoInput = document.querySelector('.caixaTextoInput');
-var texto = areaTextoInput.value.toLowerCase();
-var textoAlterado = "";
+var texto;
 var areaTextoMensagem = document.querySelector('.mensagem-caixaTexto');
 var textoMensagem = areaTextoMensagem.value.toLowerCase();
 
+var matrizCode = [['e','enter'],['i','imes'],['a','ai'],['o','ober'],['u','ufat']];
+
 function Criptografar() {
+    texto = areaTextoInput.value.toLowerCase();
     if (texto != "") {
-        textoAlterado = "";
-        texto = areaTextoInput.value.toLowerCase();
+
         document.querySelector('.mensagem-img').style.display = "none";
-
         document.querySelector('.mensagem-descricaoDestaque').style.display = "none";
-
         document.querySelector('.mensagem-descricao').style.display = "none";
 
         document.querySelector('.mensagem-caixaTexto').style.display = "block";
         document.querySelector('.mensagem-botaoCopiar').style.display = "block";
-        
-        for(let i=0; i< texto.length;i++){
-            if(texto[i]==='a'){
-                textoAlterado+= "ai"
-            }else if(texto[i]==='e'){
-                textoAlterado+= "enter"
-            }else if(texto[i]==='i'){
-                textoAlterado+= "imes"
-            }else if(texto[i]==='o'){
-                textoAlterado+= "ober"
-            }else if(texto[i]==='u'){
-                textoAlterado+= "ufat"
-            }else{
-                textoAlterado+=texto[i];
+
+        /* 
+        if (texto.includes('e')) {
+            texto = texto.replaceAll('e', 'enter');
+        }
+        if (texto.includes('i')) {
+            texto = texto.replaceAll('i', 'imes');
+        }
+        if (texto.includes('a')) {
+            texto = texto.replaceAll('a', 'ai');
+        }
+        if (texto.includes('o')) {
+            texto = texto.replaceAll('o', 'ober');
+        }
+        if (texto.includes('u')) {
+            texto = texto.replaceAll('u', 'ufat')
+        } */
+
+        for(let i=0; i<matrizCode.length;i++){
+            if(texto.includes(matrizCode[i][0])){
+                texto = texto.replaceAll(matrizCode[i][0],matrizCode[i][1]);
             }
         }
-        areaTextoMensagem.value = textoAlterado;
+
+        areaTextoMensagem.value = texto;
     }
 }
 
 function Descriptorafar() {
+    texto = areaTextoInput.value.toLowerCase();
     if (texto != "") {
         textoAlterado = "";
-        texto = areaTextoInput.value.toLowerCase();
+
         document.querySelector('.mensagem-img').style.display = "none";
 
         document.querySelector('.mensagem-descricaoDestaque').style.display = "none";
@@ -48,43 +56,61 @@ function Descriptorafar() {
 
         document.querySelector('.mensagem-caixaTexto').style.display = "block";
         document.querySelector('.mensagem-botaoCopiar').style.display = "block";
-        
-        for(let i=0; i< texto.length;i++){
-            if(texto[i]==='ai'){
-                textoAlterado+= "a"
-            }else if(texto[i]==='enter'){
-                textoAlterado+= "e"
-            }else if(texto[i]==='imes'){
-                textoAlterado+= "i"
-            }else if(texto[i]==='ober'){
-                textoAlterado+= "o"
-            }else if(texto[i]==='ufat'){
-                textoAlterado+= "ufat"
-            }else{
-                textoAlterado+=texto[i];
+
+        /* if (texto.includes('enter')) {
+            texto = texto.replaceAll('enter', 'e');
+        }
+        if (texto.includes('imes')) {
+            texto = texto.replaceAll('imes', 'i');
+        }
+        if (texto.includes('ai')) {
+            texto = texto.replaceAll('ai', 'a');
+        }
+        if (texto.includes('ober')) {
+            texto = texto.replaceAll('ober', 'o');
+        }
+        if (texto.includes('ufat')) {
+            texto = texto.replaceAll('ufat', 'u')
+        } */
+
+        for(let i=0; i<matrizCode.length;i++){
+            if(texto.includes(matrizCode[i][1])){
+                texto = texto.replaceAll(matrizCode[i][1],matrizCode[i][0]);
             }
         }
-        areaTextoMensagem.value = textoAlterado;
+
+        areaTextoMensagem.value = texto;
+        /* alert("Texto Descriptografado: " + texto); */
     }
 }
 
 
-async function Copiar(){
+async function Copiar() {
     areaTextoMensagem = document.querySelector('.mensagem-caixaTexto');
     await navigator.clipboard.writeText(areaTextoMensagem.value);
     console.log(areaTextoMensagem.value);
 }
 
-async function copiarTexto() {
-    // Obtém o elemento textarea pelo seu ID
-    var textarea = document.getElementById("meu-textarea");
-    // Copia o texto para a área de transferência
-    await navigator.clipboard.writeText(textarea.value);
-  }
+function CopiaTexto() {
+    // Get the text field
+    /* var copyText = document.getElementById("myInput"); */
+
+    // Select the text field
+    areaTextoMensagem.select();
+    //areaTextoMensagem.setSelectionRange(0, 99999); // For mobile devices
+
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(areaTextoMensagem.value);
+
+    // Alert the copied text
+    /* alert("Copied the text: " + areaTextoMensagem.value); */
+}
 
 
 var botaoCrip = document.querySelector('.botaoCriptografar');
-var botaoCopy = document.querySelector('.mensagem-descricao');
+var botaoDescrip = document.querySelector('.botaoDescriptografar')
+var botaoCopy = document.querySelector('.mensagem-botaoCopiar');
 
 botaoCrip.onclick = Criptografar;
-botaoCopy.onclick = Copiar;
+botaoCopy.onclick = CopiaTexto;
+botaoDescrip.onclick = Descriptorafar;
